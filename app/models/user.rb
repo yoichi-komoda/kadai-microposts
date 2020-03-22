ac82104c0bc8b705cce2433002c5da2ca6688ee8
yoichi.komoda <nagisasendou.biz@gmail.com>
2020-03-22 13:16:38 +0000

    favorite OK!

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favo_microposts, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -26,5 +28,22 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
+    
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)    
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(micropost)
+    self.favo_microposts.include?(micropost)
   end
 end
